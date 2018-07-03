355a5c26f3d962753afb4e573c7c1318b1959183
JeromeDrago <jerome.drago@web-atrio.com>
2018-07-03 21:30:24 +0200

    finish

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -27,17 +27,19 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  // var navbarCollapse = function() {
+  //   if ($("#mainNav").offset().top > 100) {
+  //     console.log("pipou one");
+  //     $("#mainNav").addClass("navbar-shrink");
+  //   } else {
+  //     console.log("pipou two");
+  //     $("#mainNav").removeClass("navbar-shrink");
+  //   }
+  // };
+  // // Collapse now if page is not at top
+  // navbarCollapse();
+  // // Collapse the navbar when page is scrolled
+  // $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
 
